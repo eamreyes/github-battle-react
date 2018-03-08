b602eba077f25afcf1a7d0e454f59f24329bdccf
eamreyes <eamreyes@outlook.com>
2018-03-07 19:31:55 -0800

    Add axios. Add ReposGrid. 'Life Cycle Events + Ajax'

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var api = require('../utils/api');
 
 function SelectLanguage(props){
     var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -23,13 +24,48 @@ SelectLanguage.propTypes = {
     onSelect: PropTypes.func.isRequired,
 }
 
+function RepoGrid(props) {
+    return (
+        <ul className='popular-list'>
+        {
+            props.repos.map(function (repo, index) {
+                return (
+                    <li key={repo.name} className='popular-item'>
+                        <div className='popular-rank'>#{index + 1}</div>
+                        <ul className='space-list-items'>
+                            <li>
+                                <img
+                                    className='avatar'
+                                    src={repo.owner.avatar_url}
+                                    alt={'Avatar for ' + repo.owner.login}
+                                />
+                            </li>
+                            <li>
+                                <a href={repo.html_url}>{repo.name}</a>
+                            </li>
+                            <li>@{repo.owner.login}</li>
+                            <li>{repo.stargazers_count} stars</li>
+                        </ul>
+                    </li>
+                )
+            })
+        }
+        </ul>
+    )
+}
+
+RepoGrid.propTypes = {
+    repos: PropTypes.array.isRequired
+}
+
 
 class Popular extends React.Component{
 
     constructor(props) {
         super(props); // Always pass props to React.Componenet base
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos: null,
         };
 
         // ensures that updateLanguage is always called with the correct this keyword
@@ -41,19 +77,38 @@ class Popular extends React.Component{
         this.setState(function (){
             return {
                 selectedLanguage: lang,
+                repos: null
             };
         });
+
+        // Do the AJAX
+        api.fetchPopularRepos(lang)
+            .then(function(response) {
+                this.setState(function(){
+                    return {
+                        repos: response,
+                    }
+                });
+            }.bind(this));
+    }
+
+    componentWillMount() {
+        this.updateLanguage(this.state.selectedLanguage);
     }
 
     render() {
-
-
         return (
             
             <div>
                 <SelectLanguage
                     selectedLanguage={this.state.selectedLanguage}
                     onSelect={this.updateLanguage} />
+                {
+                    !this.state.repos
+                    ? <p>LOADING</p>
+                    : <RepoGrid repos={this.state.repos} />
+                }
+                
             </div>
         )
     }
